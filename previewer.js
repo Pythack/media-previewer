94aa7cf44fc2cccf63788ae7c93b2d2e5cd01a16
Pythack <59161870+Pythack@users.noreply.github.com>
2022-12-31 17:05:57 +0100

    Add basic smart placement

--- previewer.js
+++ previewer.js
@@ -38,10 +38,25 @@ function onhoverupdate(event) {
     timeout = setTimeout(() => {
         let displayed = false;
         if (/^.*\.(png|jpg|jpeg|webp)$/.test(hovel.href.toLowerCase()) && activated) { // If URL ends with image extensions and extension activated
+            const vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0)
+            const vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0)
             previmg.src = hovel.href;
             prevdiv.style.position = "fixed";
-            prevdiv.style.top = (posy + 10).toString() + "px";
-            prevdiv.style.left = (posx + 10).toString() + "px";
+            const offset = 20;
+            if (posy >= vh/2) {
+                prevdiv.style.bottom = (vh - posy + offset).toString() + "px";
+                prevdiv.style.top = "";
+            } else {
+                prevdiv.style.top = (posy + offset).toString() + "px";
+                prevdiv.style.bottom = "";
+            }
+            if (posx >= vw/2) {
+                prevdiv.style.right = (vw - posx + offset).toString() + "px";
+                prevdiv.style.left = "";
+            } else {
+                prevdiv.style.left = (posx + offset).toString() + "px";
+                prevdiv.style.right = "";
+            }
             prevdiv.style.background = "repeating-conic-gradient(#404040 0% 25%, #ffffff 0% 50%) 50% / " + 10 + "px " + 10 + "px";
             prevdiv.style.display = "block";
             displayed = true;
