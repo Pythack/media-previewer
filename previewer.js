eaddc0adc64145431689a03173eb79a3da9cc129
Pythack <59161870+Pythack@users.noreply.github.com>
2023-01-01 15:37:21 +0100

    Add support for videos

--- previewer.js
+++ previewer.js
@@ -21,10 +21,22 @@ var prevdims = document.createElement("p");
 var previmgdiv = document.createElement("div");
 previmgdiv.id = "media-preview-img-div";
 var previmg = document.createElement("img");
-previmg.addEventListener("error", () => {
-    prevdiv.style.display = "none";
+previmg.addEventListener("error", (event) => {
+    if (event.target.style.display == "block") {
+        prevdiv.style.display = "none";
+    }
 });
+var prevvid = document.createElement("video");
+prevvid.controls = true;
+prevvid.addEventListener("error", (event) => {
+    if (event.target.style.display == "block") {
+        prevdiv.style.display = "none";
+    }
+});
+var prevvidsrc = document.createElement("source");
+prevvid.appendChild(prevvidsrc);
 previmgdiv.appendChild(previmg);
+previmgdiv.appendChild(prevvid);
 prevdiv.appendChild(prevdims);
 prevdiv.appendChild(previmgdiv);
 document.body.appendChild(prevdiv);
@@ -80,12 +92,10 @@ function onhoverupdate(event) {
     timeout = setTimeout(async () => {
         const url = hovel.href;
         let mimetype = await getMIME(url);
-        if (/^image\/(png|jpg|jpeg|webp)$/.test(mimetype.toLowerCase()) && (prevdiv.style.display == "none" || localStorage.prevlink != url) && activated) { // If URL ends with image extensions and extension activated
+        if ((prevdiv.style.display == "none" || localStorage.prevlink != url) && activated) {
             const vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0);
             const vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0);
             hidePreview();
-            previmg.src = url;
-            previmg.onload = updateDimensions;
             prevdiv.style.position = "fixed";
             const offset = 10;
             if (posy >= vh/2) {
@@ -102,9 +112,24 @@ function onhoverupdate(event) {
                 prevdiv.style.left = (posx + offset).toString() + "px";
                 prevdiv.style.right = "";
             }
-            // prevdiv.style.background = "repeating-conic-gradient(#404040 0% 25%, #ffffff 0% 50%) 50% / " + 10 + "px " + 10 + "px";
-            prevdiv.style.display = "block";
-            localStorage.prevlink = url;
+            if (/^image\/(png|jpg|jpeg|webp)$/.test(mimetype.toLowerCase())) { // If URL ends with image extensions and extension activated
+                previmg.src = url;
+                previmg.onload = updateDimensions;
+                prevvid.style.display = "none";
+                previmg.style.display = "block";
+                prevdiv.style.display = "block";
+                localStorage.prevlink = url;
+            } else if (/^video\/(mp4|x-matroska|avi)$/.test(mimetype.toLowerCase())) {
+                prevvidsrc.src = url;
+                prevvidsrc.type = mimetype;
+                prevvid.onload = updateDimensions;
+                prevdims.innerText = "";
+                previmg.style.display = "none";
+                prevvid.style.display = "block";
+                prevvid.load();
+                prevdiv.style.display = "block";
+                localStorage.prevlink = url;
+            }
         }
     }, 300);
 }
