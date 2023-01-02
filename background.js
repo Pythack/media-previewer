91b46f4162168e28d82ba009247715c5e1ac014d
Pythack <59161870+Pythack@users.noreply.github.com>
2023-01-02 19:43:56 +0100

    Add comments to background.js

--- background.js
+++ background.js
@@ -4,13 +4,13 @@ if (typeof browser === "undefined") {
 
 browser.runtime.onMessage.addListener((message, sender, sendResponse) => {
     switch(message.request) {
-        case "get-status":
+        case "get-status": // When a new page is loaded
             sendResponse(new Promise(async function (resolve, reject) {
-                let storage = await browser.storage.local.get();
-                if (storage.disactivated) {
-                    resolve(false);
+                let storage = await browser.storage.local.get(); // Get local storage to see if extension is enabled or not
+                if (storage.deactivated) { // If deactivated
+                    resolve(false); // Send back activated as false
                 } else {
-                    resolve(true);
+                    resolve(true); // Send back activated as true
                 }
             }));
             break;
@@ -18,24 +18,24 @@ browser.runtime.onMessage.addListener((message, sender, sendResponse) => {
 });
 
 function sendToAllTabs(object) {
-    browser.tabs.query({}).then((result) => {
-        result.forEach((tab) => {
-            browser.tabs.sendMessage(tab.id, object);
+    browser.tabs.query({}).then((result) => { // Get all tabs
+        result.forEach((tab) => { // For each tab
+            browser.tabs.sendMessage(tab.id, object); // Send message to tab
         });
     });
 }
 
-browser.browserAction.onClicked.addListener(() => {
-    var getstorage = browser.storage.local.get();
+browser.browserAction.onClicked.addListener(() => { // When the browser action is clicked
+    var getstorage = browser.storage.local.get(); // Get local storage
     getstorage.then(response => {
-        if (response.disactivated) {
-            browser.storage.local.set({disactivated: false});
-            sendToAllTabs({request: "update-enable",activated: true});
-            browser.browserAction.setIcon({path: browser.runtime.getURL('./iconactive.png')});
+        if (response.deactivated) { // If deactivated
+            browser.storage.local.set({deactivated: false}); // Set to false
+            sendToAllTabs({request: "update-enable",activated: true}); // Send update to tabs
+            browser.browserAction.setIcon({path: './iconactive.png'}); // Update icon
         } else {
-            browser.storage.local.set({disactivated: true});
-            sendToAllTabs({request: "update-enable",activated: false});
-            browser.browserAction.setIcon({path: browser.runtime.getURL('./icondisabled.png')});
+            browser.storage.local.set({deactivated: true}); // Set to true
+            sendToAllTabs({request: "update-enable",activated: false}); // Send update to tabs
+            browser.browserAction.setIcon({path: './icondisabled.png'}); // Update icon
         }
     });
 });
